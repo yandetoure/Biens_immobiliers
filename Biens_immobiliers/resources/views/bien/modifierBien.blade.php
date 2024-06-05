5e5c3e215eaad55ac3b2c4239440ae00a34f40d3
Girlhack007 <tourendeyeyande@gmail.com>
2024-06-05 14:14:33 +0000

    Mise à jour du chemin du boutton retour dans la page de modification du bien vers la liste des biens

--- Biens_immobiliers/resources/views/bien/modifierBien.blade.php
+++ Biens_immobiliers/resources/views/bien/modifierBien.blade.php
@@ -67,7 +67,7 @@
                 <br><br>
                 <button type="submit" class="btn btn-primary">modifier un bien</button>
                 <br><br>
-                <a href="/article" class="btn btn-danger">Retour</a>
+                <a href="/bien" class="btn btn-danger">Retour</a>
             </form>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
